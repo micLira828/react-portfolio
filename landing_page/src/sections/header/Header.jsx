7735362230ac6dc9ad584421dd1360bca34ad7fc
Michelle <micgep1015@gmail.com>
2023-02-04 22:09:42 -0800

    Started and finished the header and services section

--- landing_page/src/sections/header/Header.jsx
+++ landing_page/src/sections/header/Header.jsx
@@ -1,10 +1,25 @@
+import HeaderImage from '../../assets/header.jpg'
 import './header.css'
+import data from './data'
 
 function Header() {
   return (
-   <section id="header">
-    <h2>Header</h2>
-   </section>
+   <header id = "header">
+   <div className="container header__container">
+    <div className="header__profile">
+    <img src = {HeaderImage} alt = "Header Portrait"/>
+    </div>
+    <h3>Hajia Bintu</h3>
+    <p>You are a click away from building your dream website or web app. Send me the details of your project for a modern, mobile responsive, highly performant website today!</p>
+   <div className="header__cta">
+     <a href="#contact" className = 'btn primary'>Let's Talk</a>
+     <a href="#portfolio" className="btn light">My work</a>
+   </div>
+   <div className="header__socials">
+    {data.map(item => <a rel = "noopener noreferror"key = {item.id} target="_blank" href = {item.link} >{item.icon}</a>)}
+   </div>
+   </div>
+   </header>
   )
 }
 
--- landing_page/src/sections/services/Services.jsx
+++ landing_page/src/sections/services/Services.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import  './services.css';
+import Card from '../../components/Card';
+import data from './data';
 
 function Services() {
   return (
-    <section id="services"><h2>Services</h2></section>
+    <section id="services"><h2>My Service</h2>
+    <p>I give you the best of all my services below</p>
+  
+      <div className = "container services__container">
+       {data.map (item =>  
+        <Card className = "service light"key = {item.id}>
+        <div className="service__icon">{item.icon}</div> 
+        <div className="service__details">
+          <h4>{item.title}</h4>
+          <p>{item.desc}</p>
+        </div>
+        </Card>)};
+      </div> 
+   
+    </section>
   )
 }
 
