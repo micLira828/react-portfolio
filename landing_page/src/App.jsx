21ffa24f9839af4cfc26d4978ec6f8b15e1bf441
Michelle <micgep1015@gmail.com>
2023-02-28 18:51:57 -0800

    Added the computer and header sm links

--- landing_page/src/App.jsx
+++ landing_page/src/App.jsx
@@ -7,16 +7,52 @@ import Testimonials from './sections/testimonials/Testimonials';
 import FAQs from './sections/FAQS/FAQs';
 import Contact from './sections/contact/Contact';
 import Footer from './sections/footer/Footer';
+import { useEffect } from 'react';
+import Computer from './sections/computer/Computer';
+import $ from 'jquery';
 //import FloatingNav from './sections/floating-nav/FloatingNav';
 
 
 
 const App = () => {
- 
 
-  
+  useEffect(() => {
+   
+    $('#rightCircle').hide();
+     $('#leftCircle').hide();
+     $('#middleCircle').hide();
+     $('#mouse_pointer').hide();
+     $('.ball').hide();
+     $('main').hide();
+    
+
+   $('#EllipseNonGradient').click(
+     function(e){
+       
+      $('#mouse_pointer').fadeIn(1000, function(){
+         $('.clickMe').fadeOut(1000, function(){
+            
+             $('#leftCircle').fadeIn(1000,function(){
+             $('#middleCircle').fadeIn(1000, function(){
+             $('#rightCircle').fadeIn(1000, function(){
+              $('#computerAnimation').slideUp(2000, function(){
+                $('main').slideDown(3500);
+              });
+              
+            
+             });
+           });
+           });
+      })
+     });
+   
+   });
+});
 
   return (
+
+    <>
+    <Computer/>
     <main>
         <Navbar/>
         <Header/>
@@ -29,6 +65,7 @@ const App = () => {
         <Footer/>
       
     </main>
+    </>
   )
 }
 
--- landing_page/src/sections/header/data.js
+++ landing_page/src/sections/header/data.js
@@ -4,10 +4,10 @@ import {AiOutlineDribbble} from 'react-icons/ai'
 import {AiFillGithub} from 'react-icons/ai'
 
 const data = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
-    {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
-    {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
+    {id: 1, link: 'https://www.instagram.com/michellelgg6/', icon: <AiOutlineInstagram/>},
+    {id: 2, link: 'https://twitter.com/MichelleGepsht1', icon: <AiOutlineTwitter/>},
+    {id: 3, link: 'https://dribbble.com/michellelgg6', icon: <AiOutlineDribbble/>},
+    {id: 4, link: 'https://github.com/micLira828', icon: <AiFillGithub/>}
 ]
 
 export default data
